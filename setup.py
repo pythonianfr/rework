c9a6a5321b4fef98973544cf271922fc52aeb880
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2017-11-09 16:23:11 +0100

    pkg: add trove classifiers

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='rework',
       author='Aurelien Campeas',
       author_email='aurelien.campeas@pythonian.fr',
       description='A database-oriented distributed task dispatcher',
+      url='https://bitbucket.org/pythonian/rework',
       packages=['rework'],
       install_requires=[
           'pytest_sa_pg',
@@ -20,5 +21,16 @@ setup(name='rework',
           'console_scripts': [
               'rework=rework.cli:rework'
           ]
-      }
+      },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Database',
+          'Topic :: System :: Distributed Computing',
+          'Topic :: Software Development :: Object Brokering'
+      ]
 )
