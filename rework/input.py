abdebb9c1cb45762da1cf9962612b7665c7edfc8
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-04-20 13:48:21 +0200

    rework/input: deprecate the module

--- rework/input.py
+++ rework/input.py
@@ -1,7 +1,16 @@
+from warnings import warn
+
 from rework.io import (
     number,
     string,
     file,
     datetime,
     moment
+)
+
+warn(
+    'the `input` module is deprecated, use `io` instead, '
+    'as in "from rework.io import ..."',
+    DeprecationWarning,
+    stacklevel=2
 )
