af3b912a244132248144dbced3a29af324ffea11
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-09-21 18:34:40 +0200

    pkg: prepare 0.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ doc = Path(__file__).parent / 'README.md'
 
 
 setup(name='rework',
-      version='0.12.0',
+      version='0.13.0',
       author='Aurelien Campeas',
       author_email='aurelien.campeas@pythonian.fr',
       description='A database-oriented distributed task dispatcher',
